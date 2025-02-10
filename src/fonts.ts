ea022d5e3b40e84579585e52f3d0d1ac43c38911
Arthur K <iamarthurk@hey.com>
2025-02-11 03:52:54 +0400

    chore: configure fonts, color palette and remove react-hook-form

---
+++ src/fonts.ts
@@ -0,0 +1,10 @@
+import { Sen, Libre_Caslon_Text } from 'next/font/google';
+
+export const sen = Sen({
+  subsets: ["latin"],
+});
+
+export const libreCaslonText = Libre_Caslon_Text({
+  subsets: ["latin"],
+  weight: '400'
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  // Scan src/components directory for styles
+  content: ["./src/components/**/*.{ts,tsx}"],
+
+  // Theme colors based on figma
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      green: "#006F62",
+      brown: "#5A3A27",
+      black: "#1D1F22",
+      red: "#B3261E"
+    }
+  },
+
   plugins: [],
 } satisfies Config;
